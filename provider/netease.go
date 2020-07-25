8629023024865668270d91c7f0e71944051b175c
Fan Yang <me@minidump.info>
2020-07-25 17:13:57 +0800

    adjust api url

--- provider/netease.go
+++ provider/netease.go
@@ -24,8 +24,9 @@ const (
 	neteaseEAPIKey                     = "e82ckenh8dichen8"
 	neteaseDefaultRSAPublicKeyModulus  = "e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
 	neteaseDefaultRSAPublicKeyExponent = 0x10001
-	neteaseAPIGetSongsURL              = "https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token="
+	neteaseAPIGetSongsURL              = "http://music.163.com/weapi/song/enhance/player/url/v1?csrf_token="
 	neteaseAPISearch                   = `http://music.163.com/api/search/pc`
+	neteaseAPIGetLyric                 = `http://music.163.com/weapi/song/lyric?csrf_token=`
 )
 
 func weapi(origData interface{}) map[string]interface{} {

4a35064cadf5405fa3c4f25d767c51aad3d6048e
Fan Yang <missdeer@dfordsoft.com>
2020-07-28 21:59:47 +0800

    (*)fix bass on windows

--- output/bass/bass.go
+++ output/bass/bass.go
@@ -128,7 +128,7 @@ func Pause() int {
 
 // SetDevice Sets the device to use for subsequent calls in the current thread.
 func SetDevice(device C.uint) int {
-	return int(C.BASS_SetDevice(C.uint(device)))
+	return int(C.BASS_SetDevice(C.DWORD(device)))
 }
 
 func SetVolume(value float32) bool {
@@ -140,13 +140,13 @@ func SetVolume(value float32) bool {
 }
 
 func SetChanAttr(handle uint, attr uint, value float32) uint {
-	r := uint(C.BASS_ChannelSetAttribute(C.uint(handle), C.uint(attr), C.float(value)))
+	r := uint(C.BASS_ChannelSetAttribute(C.DWORD(handle), C.DWORD(attr), C.float(value)))
 	return r
 }
 
 func GetChanAttr(handle uint, attr uint) float32 {
 	var value float32
-	C.BASS_ChannelGetAttribute(C.uint(handle), C.uint(attr), (*C.float)(unsafe.Pointer(&value)))
+	C.BASS_ChannelGetAttribute(C.DWORD(handle), C.DWORD(attr), (*C.float)(unsafe.Pointer(&value)))
 	return value
 }
 
@@ -173,26 +173,26 @@ func GetCPU() float32 {
 
 // StreamCreate Creates a user sample stream.
 func StreamCreate(freq uint, proc *C.STREAMPROC, user unsafe.Pointer) uint {
-	return uint(C.BASS_StreamCreate(C.uint(freq), 2, C.BASS_SAMPLE_FLOAT, proc, user))
+	return uint(C.BASS_StreamCreate(C.DWORD(freq), 2, C.BASS_SAMPLE_FLOAT, proc, user))
 }
 
 // StreamCreateFile Creates a sample stream from an MP3, MP2, MP1, OGG, WAV, AIFF or plugin supported file.
 func StreamCreateFile(mem int, file string, offset uint64, length uint64) uint {
-	return uint(C.BASS_StreamCreateFile(C.int(mem), unsafe.Pointer(C.CString(file)), C.ulonglong(offset), C.ulonglong(length), C.uint(C.BASS_SAMPLE_FLOAT)))
+	return uint(C.BASS_StreamCreateFile(C.int(mem), unsafe.Pointer(C.CString(file)), C.ulonglong(offset), C.ulonglong(length), C.DWORD(C.BASS_SAMPLE_FLOAT)))
 }
 
 // StreamCreateURL ates a sample stream from an MP3, MP2, MP1, OGG, WAV, AIFF or plugin supported file on the internet, optionally receiving the downloaded data in a callback function.
 func StreamCreateURL(url string, offset uint, proc *C.DOWNLOADPROC, user unsafe.Pointer) uint {
-	return uint(C.BASS_StreamCreateURL(C.CString(url), C.uint(offset), C.BASS_SAMPLE_FLOAT, proc, user))
+	return uint(C.BASS_StreamCreateURL(C.CString(url), C.DWORD(offset), C.BASS_SAMPLE_FLOAT, proc, user))
 }
 
 func StreamPutData(handle uint, buffer []byte, length int) uint32 {
 
-	return uint32(C.BASS_StreamPutData(C.uint(handle), C.CBytes(buffer), C.uint(length)))
+	return uint32(C.BASS_StreamPutData(C.DWORD(handle), C.CBytes(buffer), C.DWORD(length)))
 }
 
 func StreamFree(handle uint) uint32 {
-	return uint32(C.BASS_StreamFree(C.uint(handle)))
+	return uint32(C.BASS_StreamFree(C.DWORD(handle)))
 }
 
 // --------------------------------------
@@ -201,57 +201,57 @@ func StreamFree(handle uint) uint32 {
 
 // ChannelPlay Starts (or resumes) playback of a sample, stream, MOD music, or recording.
 func ChannelPlay(handle uint, restart int) int {
-	return int(C.BASS_ChannelPlay(C.uint(handle), C.int(restart)))
+	return int(C.BASS_ChannelPlay(C.DWORD(handle), C.int(restart)))
 }
 
 // ChannelPause a sample, stream, MOD music, or recording.
 func ChannelPause(handle uint) int {
-	return int(C.BASS_ChannelPause(C.uint(handle)))
+	return int(C.BASS_ChannelPause(C.DWORD(handle)))
 }
 
 // ChannelStop Stops a sample, stream, MOD music, or recording.
 func ChannelStop(handle uint) int {
-	return int(C.BASS_ChannelStop(C.uint(handle)))
+	return int(C.BASS_ChannelStop(C.DWORD(handle)))
 }
 
 // ChannelBytes2Seconds Translates a byte position into time (seconds), based on a channel's format.
 func ChannelBytes2Seconds(handle uint, pos int) int {
-	return int(C.BASS_ChannelBytes2Seconds(C.uint(handle), C.ulonglong(pos)))
+	return int(C.BASS_ChannelBytes2Seconds(C.DWORD(handle), C.ulonglong(pos)))
 }
 
 // ChannelSeconds2Bytes Translates a time (seconds) position into bytes, based on a channel's format.
 func ChannelSeconds2Bytes(handle uint, pos int) int {
-	return int(C.BASS_ChannelSeconds2Bytes(C.uint(handle), C.double(pos)))
+	return int(C.BASS_ChannelSeconds2Bytes(C.DWORD(handle), C.double(pos)))
 }
 
 // ChannelIsActive Checks if a sample, stream, or MOD music is active (playing) or stalled. Can also check if a recording is in progress.
 func ChannelIsActive(handle uint) int {
-	return int(C.BASS_ChannelIsActive(C.uint(handle)))
+	return int(C.BASS_ChannelIsActive(C.DWORD(handle)))
 }
 
 // ChannelGetPosition Retrieves the playback position of a sample, stream, or MOD music. Can also be used with a recording channel.
 func ChannelGetPosition(handle uint, mode int) int {
-	return int(C.BASS_ChannelGetPosition(C.uint(handle), C.uint(mode)))
+	return int(C.BASS_ChannelGetPosition(C.DWORD(handle), C.DWORD(mode)))
 }
 
 // ChannelSetPosition Sets the playback position of a sample, MOD music, or stream.
 func ChannelSetPosition(handle uint, pos int, mode int) int {
-	return int(C.BASS_ChannelSetPosition(C.uint(handle), C.ulonglong(pos), C.uint(mode)))
+	return int(C.BASS_ChannelSetPosition(C.DWORD(handle), C.ulonglong(pos), C.DWORD(mode)))
 }
 
 // ChannelSetAttribute Sets the value of a channel's attribute.
 func ChannelSetAttribute(handle uint, attrib C.uint, value C.float) int {
-	return int(C.BASS_ChannelSetAttribute(C.uint(handle), C.uint(attrib), value))
+	return int(C.BASS_ChannelSetAttribute(C.DWORD(handle), C.DWORD(attrib), value))
 }
 
 // ChannelUpdate Updates the playback buffer of a stream or MOD music.
 func ChannelUpdate(handle uint, length C.uint) int {
-	return int(C.BASS_ChannelUpdate(C.uint(handle), C.uint(length)))
+	return int(C.BASS_ChannelUpdate(C.DWORD(handle), C.DWORD(length)))
 }
 
 // ChannelGetLength Retrieves the playback length of a channel.
 func ChannelGetLength(handle uint, mode int) int {
-	return int(C.BASS_ChannelGetLength(C.uint(handle), C.uint(mode)))
+	return int(C.BASS_ChannelGetLength(C.DWORD(handle), C.DWORD(mode)))
 }
 
 // ---------------------------------------
@@ -261,7 +261,7 @@ const (
 
 // PluginLoad load a bass plugin
 func PluginLoad(file string) int {
-	result := int(C.BASS_PluginLoad(C.CString(file), C.uint(0)))
+	result := int(C.BASS_PluginLoad(C.CString(file), C.DWORD(0)))
 	switch result {
 	case BASS_ERROR_FILEOPEN:
 		log.Println(file, "err: BASS_ERROR_FILEOPEN")
@@ -278,7 +278,7 @@ func PluginLoad(file string) int {
 
 // PluginFree free a bass plugin
 func PluginFree(handle int) bool {
-	result := int(C.BASS_PluginFree(C.uint(handle)))
+	result := int(C.BASS_PluginFree(C.DWORD(handle)))
 	if result == 0 {
 		return false
 	}

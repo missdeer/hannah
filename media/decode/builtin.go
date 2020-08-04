f726c4edd975991cc34935a9fff7ce9afb1c3e97
Fan Yang <missdeer@dfordsoft.com>
2020-08-04 22:00:13 +0800

    (*)always use mpg123 as the decoder

--- media/decode/builtin.go
+++ media/decode/builtin.go
@@ -6,29 +6,20 @@ import (
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/flac"
-	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/vorbis"
 	"github.com/faiface/beep/wav"
-
-	"github.com/missdeer/hannah/config"
 )
 
 type builtinDecoder func(io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error)
 
 var (
 	builtinDecoderMap = map[string]builtinDecoder{
-		".mp3": func(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) {
-			if config.Mpg123 {
-				return Mpg123Decode(r)
-			} else {
-				return mp3.Decode(r)
-			}
-		},
-		".ogg":  func(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) { return vorbis.Decode(r) },
+		".mp3":  Mpg123Decode,
+		".ogg":  vorbis.Decode,
 		".flac": func(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) { return flac.Decode(r) },
 		".wav":  func(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) { return wav.Decode(r) },
-		".m4a":  func(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) { return FAADDecode(r) },
-		".aac":  func(r io.ReadCloser) (beep.StreamSeekCloser, beep.Format, error) { return FAADDecode(r) },
+		".m4a":  FAADDecode,
+		".aac":  FAADDecode,
 	}
 )
 

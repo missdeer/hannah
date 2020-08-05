fc3b135fd4882ab1c88d03f6adb52cbb3fb28a81
Fan Yang <fyang3@cisco.com>
2020-08-05 14:17:39 +0800

    (+)support kuwo playlist

--- provider/kuwo.go
+++ provider/kuwo.go
@@ -1,17 +1,21 @@
 package provider
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 
+	"github.com/missdeer/hannah/config"
 	"github.com/missdeer/hannah/util"
 )
 
 const (
-	kuwoAPISearch     = `http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?key=%s&pn=%d&rn=%d`
-	kuwoAPIToken      = `http://www.kuwo.cn/search/list?key=`
-	kuwoAPIConvertURL = `http://antiserver.kuwo.cn/anti.s?type=convert_url&format=mp3|wma|aac&response=url&rid=%s`
+	kuwoAPISearch         = `http://www.kuwo.cn/api/www/search/searchMusicBykeyWord?key=%s&pn=%d&rn=%d`
+	kuwoAPIToken          = `http://www.kuwo.cn/search/list?key=`
+	kuwoAPIConvertURL     = `http://antiserver.kuwo.cn/anti.s?type=convert_url&format=mp3|wma|aac&response=url&rid=%s`
+	kuwoAPIHot            = `http://www.kuwo.cn/www/categoryNew/getPlayListInfoUnderCategory?type=taglist&digest=10000&id=37&start=%d&count=%d`
+	kuwoAPIPlaylistDetail = `http://nplserver.kuwo.cn/pl.svc?op=getlistinfo&pn=0&rn=200&encode=utf-8&keyset=pl2012&pcmp4=1&pid=%s&vipver=MUSIC_9.0.2.0_W1&newver=1`
 )
 
 type kuwo struct {
@@ -163,12 +167,141 @@ func (p *kuwo) ResolveSongLyric(song Song) (Song, error) {
 	return song, nil
 }
 
-func (p *kuwo) HotPlaylist(page int) (Playlists, error) {
-	return nil, nil
+type kuwoHotPlaylists struct {
+	Data []struct {
+		Img   string `json:"img"`
+		Total string `json:"total"`
+		Data  []struct {
+			Img   string `json:"img"`
+			UName string `json:"uname"`
+			Name  string `json:"name"`
+			UID   string `json:"uid"`
+			Total string `json:"total"`
+			ID    string `json:"id"`
+		} `json:"data"`
+		Start string `json:"start"`
+		Count string `json:"count"`
+		Name  string `json:"name"`
+		ID    string `json:"id"`
+		Type  string `json:"type"`
+	} `json:"data"`
+	Msg    string `json:"msg"`
+	RegID  string `json:"regid"`
+	Status int    `json:"status"`
+}
+
+func (p *kuwo) HotPlaylist(page int) (res Playlists, err error) {
+	u := fmt.Sprintf(kuwoAPIHot, (page-1)*config.Limit+1, config.Limit)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Referer", "http://www.kuwo.cn/")
+	req.Header.Set("Origin", "http://www.kuwo.cn/")
+	req.Header.Set("Accept-Language", "zh-CN,zh-HK;q=0.8,zh-TW;q=0.6,en-US;q=0.4,en;q=0.2")
+	req.Header.Set("Accept-Encoding", "gzip, deflate")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, ErrStatusNotOK
+	}
+
+	content, err := util.ReadHttpResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var hot kuwoHotPlaylists
+	if err = json.Unmarshal(content, &hot); err != nil {
+		return nil, err
+	}
+
+	if len(hot.Data) == 0 {
+		return nil, errors.New("empty playlist")
+	}
+
+	for _, d := range hot.Data[0].Data {
+		res = append(res, Playlist{
+			ID:       d.ID,
+			Title:    d.Name,
+			Provider: "kuwo",
+			URL:      fmt.Sprintf(`http://kuwo.cn/playlist_detail/%s`, d.ID),
+		})
+	}
+
+	return res, nil
+}
+
+type kuwoPlaylistDetail struct {
+	ID        int    `json:"id"`
+	Info      string `json:"info"`
+	Pic       string `json:"pic"`
+	Title     string `json:"title"`
+	Total     int    `json:"total"`
+	MusicList []struct {
+		ID     string `json:"id"`
+		Format string `json:"format"`
+		Artist string `json:"artist"`
+		Name   string `json:"name"`
+	} `json:"musiclist"`
 }
 
 func (p *kuwo) PlaylistDetail(pl Playlist) (Songs, error) {
-	return nil, nil
+	u := fmt.Sprintf(kuwoAPIPlaylistDetail, pl.ID)
+
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Referer", "http://www.kuwo.cn/")
+	req.Header.Set("Origin", "http://www.kuwo.cn/")
+	req.Header.Set("Accept-Language", "zh-CN,zh-HK;q=0.8,zh-TW;q=0.6,en-US;q=0.4,en;q=0.2")
+	req.Header.Set("Accept-Encoding", "gzip, deflate")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, ErrStatusNotOK
+	}
+
+	content, err := util.ReadHttpResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var pld kuwoPlaylistDetail
+	if err = json.Unmarshal(content, &pld); err != nil {
+		return nil, err
+	}
+
+	var songs Songs
+	for _, p := range pld.MusicList {
+		songs = append(songs, Song{
+			ID:       p.ID,
+			Title:    p.Name,
+			Artist:   p.Artist,
+			Provider: "kuwo",
+		})
+	}
+
+	return songs, nil
 }
 
 func (p *kuwo) Name() string {

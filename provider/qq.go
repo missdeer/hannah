9e03c12107cbb718174874ccc32de5bd94d8ada3
Fan Yang <missdeer@dfordsoft.com>
2020-08-06 14:26:27 +0800

    qq hot/playlist finished

--- provider/qq.go
+++ provider/qq.go
@@ -1,8 +1,10 @@
 package provider
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -194,16 +196,156 @@ func (p *qq) ResolveSongURL(song Song) (Song, error) {
 	return song, nil
 }
 
-func (p *qq) ResolveSongLyric(song Song) (Song, error){
+func (p *qq) ResolveSongLyric(song Song) (Song, error) {
 	return song, nil
 }
 
-func (p *qq) HotPlaylist(page int) (Playlists, error) {
-	return nil, nil
+type qqHot struct {
+	Code int `json:"code"`
+	Data struct {
+		SIN  int `json:"sin"`
+		EIN  int `json:"ein"`
+		List []struct {
+			DissID   string `json:"dissid"`
+			DissName string `json:"dissname"`
+			ImgURL   string `json:"imgurl`
+		} `json:"list"`
+	} `json:"data"`
+}
+
+func (p *qq) HotPlaylist(page int, limit int) (Playlists, error) {
+	// https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg?picmid=1&rnd=%d&g_tk=732560869&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0&categoryId=10000000&sortId=5&sin=%d&ein=%d
+	u := fmt.Sprintf(`https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg?picmid=1&rnd=%d&g_tk=732560869&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0&categoryId=10000000&sortId=5&sin=%d&ein=%d`,
+		rand.Int63(), page*limit, (page+1)*limit-1)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Referer", "http://y.qq.com/")
+	req.Header.Set("Origin", "http://y.qq.com/")
+	req.Header.Set("Accept-Language", "zh-CN,zh-HK;q=0.8,zh-TW;q=0.6,en-US;q=0.4,en;q=0.2")
+	req.Header.Set("Accept-Encoding", "gzip, deflate")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, ErrStatusNotOK
+	}
+
+	content, err := util.ReadHttpResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	var hot qqHot
+	if err = json.Unmarshal(content, &hot); err != nil {
+		return nil, err
+	}
+
+	var pls Playlists
+	for _, pl := range hot.Data.List {
+		pls = append(pls, Playlist{
+			ID:       pl.DissID,
+			Title:    pl.DissName,
+			Image:    pl.ImgURL,
+			Provider: "qq",
+		})
+	}
+
+	return pls, nil
+}
+
+type qqPlaylistDetail struct {
+	Code   int `json:"code"`
+	CDList []struct {
+		DissTID  string `json:"disstid"`
+		DissID   int    `json:"dissid"`
+		DissName string `json:"DissName"`
+		Logo     string `json:"logo"`
+		SongList []struct {
+			AlbumID   int    `json:"albumid"`
+			AlbumName string `json:"albumname"`
+			Singer    []struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+			} `json:"singer"`
+			SongID   int    `json:"songid"`
+			SongMID  string `json:"songmid"`
+			SongName string `json:"songname"`
+		} `json:"songlist"`
+	} `json:"cdlist"`
 }
 
 func (p *qq) PlaylistDetail(pl Playlist) (Songs, error) {
-	return nil, nil
+	// http://i.y.qq.com/qzone-music/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0&nosign=1&disstid=%s&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=yqq&jsonpCallback=jsonCallback&needNewCode=0
+	u := fmt.Sprintf(`http://i.y.qq.com/qzone-music/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0&nosign=1&disstid=%s&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=GB2312&outCharset=utf-8&notice=0&platform=yqq&jsonpCallback=jsonCallback&needNewCode=0`,
+		pl.ID)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Referer", "http://y.qq.com/")
+	req.Header.Set("Origin", "http://y.qq.com/")
+	req.Header.Set("Accept-Language", "zh-CN,zh-HK;q=0.8,zh-TW;q=0.6,en-US;q=0.4,en;q=0.2")
+	req.Header.Set("Accept-Encoding", "gzip, deflate")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, ErrStatusNotOK
+	}
+
+	content, err := util.ReadHttpResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if bytes.HasPrefix(content, []byte(`jsonCallback(`)) && bytes.HasSuffix(content, []byte(`)`)) {
+		content = content[len(`jsonCallback(`):]
+		content = content[:len(content)-1]
+	}
+
+	var pld qqPlaylistDetail
+	if err = json.Unmarshal(content, &pld); err != nil {
+		return nil, err
+	}
+
+	if len(pld.CDList) == 0 {
+		return nil, errors.New("empty playlist")
+	}
+
+	var songs Songs
+	for _, pl := range pld.CDList[0].SongList {
+		var singers []string
+		for _, s := range pl.Singer {
+			singers = append(singers, s.Name)
+		}
+		songs = append(songs, Song{
+			ID:       pl.SongMID,
+			Title:    pl.SongName,
+			Image:    pld.CDList[0].Logo,
+			Artist:   strings.Join(singers, "/"),
+			Provider: "qq",
+		})
+	}
+
+	return songs, nil
 }
 
 func (p *qq) Name() string {

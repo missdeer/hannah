59ae3bb4987401bb514bb9a4da57c1121e509d4f
Fan Yang <missdeer@dfordsoft.com>
2020-08-16 12:53:35 +0800

    (*)modify returned file name

--- rp/search.go
+++ rp/search.go
@@ -36,7 +36,7 @@ func searchSongs(c *gin.Context) {
 	if config.CacheEnabled && refresh != "1" {
 		b, err := redis.GetBytes(urlKey)
 		if err == nil {
-			c.Writer.Header().Set(`Content-Disposition`, `attachment; filename="playlist.m3u"`)
+			c.Writer.Header().Set(`Content-Disposition`, `attachment; filename="searchresult.m3u"`)
 			c.Data(http.StatusOK, "audio/x-mpegurl", b)
 			return
 		}
@@ -91,6 +91,6 @@ func searchSongs(c *gin.Context) {
 		redis.Put(urlKey, b.Bytes())
 	}
 
-	c.Writer.Header().Set(`Content-Disposition`, `attachment; filename="playlist.m3u"`)
+	c.Writer.Header().Set(`Content-Disposition`, `attachment; filename="searchresult.m3u"`)
 	c.Data(http.StatusOK, "audio/x-mpegurl", b.Bytes())
 }

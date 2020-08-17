cb4b1e20ce0bef7c500bff6ed4d4ca06734ef2d5
Fan Yang <fyang3@cisco.com>
2020-08-17 16:21:25 +0800

    (+)add netease artist match

--- rp/m3ulink.go
+++ rp/m3ulink.go
@@ -41,6 +41,7 @@ var (
 	}
 	artistPatterns = map[*regexp.Regexp]string{
 		regexp.MustCompile(`^https?:\/\/music.163.com\/weapi\/v1\/artist\/([0-9]+)`):                                                "netease",
+		regexp.MustCompile(`^https?:\/\/music.163.com\/#\/artist\?id=([0-9]+)`):                                                     "netease",
 		regexp.MustCompile(`^https?:\/\/y.qq.com\/n\/yqq\/singer\/([0-9a-zA-Z]+)\.html`):                                            "qq",
 		regexp.MustCompile(`^https?:\/\/www.xiami.com\/artist\/([0-9a-zA-Z]+)`):                                                     "xiami",
 		regexp.MustCompile(`^https?:\/\/www.xiami.com\/list\?scene=artist&type=[0-9a-zA-Z]+&query={%22artistId%22:%22([0-9]+)%22}`): "xiami",

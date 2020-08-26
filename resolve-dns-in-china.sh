fcbb4269a232d0c156ba28a06e3bb0c4e71ad4e0
Fan Yang <fyang3@cisco.com>
2020-08-26 16:32:18 +0800

    (*)remove duplicate host records

--- resolve-dns-in-china.sh
+++ resolve-dns-in-china.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-grep -r -a 'https\?:\/\/[a-zA-Z0-9\.]\+\/' -o --include='*.go' . | awk -F '//' '{print $2}' | sort -n | uniq | awk -F '/' '{print $1}' | while read domain
+cat /etc/hosts | grep "\(.migu.cn\|.163.com\|.kugou.com\|kuwo.cn\|.xiami.com\|.qq.com\|.kgimg.com\|.bilibili.com\)$" | while read line; do echo $line | awk -F ' ' '{print $2}'; done > domainlist.txt
+grep -r -a 'https\?:\/\/[a-zA-Z0-9\.]\+\/' -o --include='*.go' . | awk -F '//' '{print $2}' | awk -F '/' '{print $1}' | grep "\(.migu.cn\|.163.com\|.kugou.com\|kuwo.cn\|.xiami.com\|.qq.com\|.kgimg.com\|.bilibili.com\)$"  >>domainlist.txt 
+cat domainlist.txt | sort -n | uniq | while read domain
 do 
     ip=`curl "http://119.29.29.29/d?dn=$domain" -s $1 $2 $3`; 
     firstip=`echo $ip | awk -F ';' '{print $1}'`;  
@@ -7,4 +9,6 @@ do
     then
         echo $firstip $domain; 
     fi
-done
+done > result.txt
+cat result.txt | sort -n
+rm -f domainlist.txt result.txt 

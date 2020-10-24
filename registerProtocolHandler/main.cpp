646312294f7fab4417db93c0b6de35803596cf25
Fan Yang <missdeer@dfordsoft.com>
2020-10-24 10:09:52 +0800

    (+)add project to register protocol handler for Hannah

---
+++ registerProtocolHandler/main.cpp
@@ -0,0 +1,21 @@
+#include <QCoreApplication>
+#include <QDir>
+#include <QSettings>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+#if defined(Q_OS_WIN)
+    QSettings mxKey("HKEY_CLASSES_ROOT\\hannah", QSettings::NativeFormat);
+    mxKey.setValue(".", "URL:hannah Protocol");
+    mxKey.setValue("URL Protocol", "");
+    mxKey.sync();
+
+    QSettings mxOpenKey("HKEY_CLASSES_ROOT\\hannah\\shell\\open\\command", QSettings::NativeFormat);
+    mxOpenKey.setValue(".", QChar('"') + QDir::toNativeSeparators(QCoreApplication::applicationDirPath()) + QString("\\Hannah.exe\" \"%1\""));
+    mxKey.sync();
+#endif
+
+    return a.exec();
+}

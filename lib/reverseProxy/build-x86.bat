28131e5fc63ae12fd578c7da2c9bef9ca2ce169c
Fan Yang <fyang3@cisco.com>
2020-10-27 14:05:10 +0800

    (*)don't build rp.a twice

--- lib/reverseProxy/build-x86.bat
+++ lib/reverseProxy/build-x86.bat
@@ -1,9 +1,5 @@
 gcc -v
-go version
-go env
-set GOARCH=386
-set CGO_ENABLED=1
-go build -buildmode=c-archive -o rp.a 
+ren librp.a rp.a
 gcc rp.def rp.a -shared -lwinmm -lWs2_32 -o rp.dll -Wl,--out-implib,rp.dll.a
 lib /def:rp.def /name:rp.dll /out:rp.lib /MACHINE:X86
 del /q *.exp

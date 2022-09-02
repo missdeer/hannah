c267e846fdfa6422da74d1332c855f240e2cb588
Fan Yang <missdeer@gmail.com>
2022-09-02 13:34:28 +0800

    string literal

--- desktop/comboboxdelegate.cpp
+++ desktop/comboboxdelegate.cpp
@@ -5,18 +5,18 @@ ComboBoxDelegate::ComboBoxDelegate(QObject *parent) : QItemDelegate(parent) {}
 
 void ComboBoxDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    if (index.data(Qt::AccessibleDescriptionRole).toString() == QLatin1String("separator"))
+    if (index.data(Qt::AccessibleDescriptionRole).toString() == QStringLiteral("separator"))
     {
         painter->setPen(Qt::gray);
         painter->drawLine(option.rect.left(), option.rect.center().y(), option.rect.right(), option.rect.center().y());
     }
-    else if (index.data(Qt::AccessibleDescriptionRole).toString() == QLatin1String("parent"))
+    else if (index.data(Qt::AccessibleDescriptionRole).toString() == QStringLiteral("parent"))
     {
         QStyleOptionViewItem parentOption = option;
         parentOption.state |= QStyle::State_Enabled;
         QItemDelegate::paint(painter, parentOption, index);
     }
-    else if (index.data(Qt::AccessibleDescriptionRole).toString() == QLatin1String("child"))
+    else if (index.data(Qt::AccessibleDescriptionRole).toString() == QStringLiteral("child"))
     {
         QStyleOptionViewItem childOption = option;
         int                  indent      = option.fontMetrics.horizontalAdvance(QString(4, QChar(' ')));
@@ -32,7 +32,7 @@ void ComboBoxDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opti
 
 QSize ComboBoxDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    if (index.data(Qt::AccessibleDescriptionRole).toString() == QLatin1String("separator"))
+    if (index.data(Qt::AccessibleDescriptionRole).toString() == QStringLiteral("separator"))
         return QSize(0, 10);
     return QItemDelegate::sizeHint(option, index);
 }
